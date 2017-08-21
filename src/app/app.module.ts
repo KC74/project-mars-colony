019571dbdf8a56b9079b85f3c421a4e8452af5b6
ShaahidKhalpey <sk@Mys-MacBook-Pro.local>
2017-08-21 15:12:33 -0700

    mars colony boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomComponent } from './welcom/welcom.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RegisterComponent } from './register/register.component';
+import { EncountersComponent } from './encounters/encounters.component';
+import { ReportComponent } from './report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    RegisterComponent,
+    EncountersComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule
