1a2567325bd5fcd8bf72d4a290deeaf322e6f5cc
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-08-24 13:25:04 -0700

    imported browser animations module into angular.

--- src/app/components/encounters/encounters.component.ts
+++ src/app/components/encounters/encounters.component.ts
@@ -7,7 +7,12 @@ import { Report } from '../../models/report'
   selector: 'app-encounters',
   templateUrl: './encounters.component.html',
   providers:[EncountersService],
-  styles: []
+  styles: [`
+  * { box-sizing: border-box ;}
+  .container { height: 100%; width: 100%; }
+  .encounters-title { 10vh }
+  .main-body { width: 100%; height: 60vh; overflow: scroll; }
+  `]
 })
 export class EncountersComponent implements OnInit {
 
@@ -16,13 +21,19 @@ export class EncountersComponent implements OnInit {
   constructor(private encountersService: EncountersService) { }
 
   /**
+   * -------------------------
    * Syntatic Sugar
+   * -------------------------
    * 
+   *  Insert reason why here.
+   * 
+   * -------------------------
    * ngOnInit() {
    *    this.encountersService.getEncounters().then((response) => {
    *    console.log(response);
    *   });
    * }
+   * -------------------------
    */
   async ngOnInit() {
     this.encounters = await this.encountersService.getEncounters(); 
